496f93e31dfaab4bd9119c139b27964ab476cdbf
Auchus <100021326+Auchus@users.noreply.github.com>
2022-12-11 16:48:48 +0700

    text

---
+++ avito/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace avito
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.id_txt = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(54, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(668, 207);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(259, 270);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(33, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Цена";
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(57, 305);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(100, 20);
+            this.name_txt.TabIndex = 3;
+            // 
+            // id_txt
+            // 
+            this.id_txt.Location = new System.Drawing.Point(262, 305);
+            this.id_txt.Name = "id_txt";
+            this.id_txt.Size = new System.Drawing.Size(100, 20);
+            this.id_txt.TabIndex = 4;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(463, 302);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(103, 23);
+            this.add.TabIndex = 5;
+            this.add.Text = "Добавить";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // edit
+            // 
+            this.edit.Location = new System.Drawing.Point(463, 348);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(103, 23);
+            this.edit.TabIndex = 6;
+            this.edit.Text = "Редактирование";
+            this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(463, 387);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(103, 23);
+            this.delete.TabIndex = 7;
+            this.delete.Text = "Удалить";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.edit);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.id_txt);
+            this.Controls.Add(this.name_txt);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox id_txt;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ avito/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace avito
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id_txt.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            name_txt.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+        }
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source=data_table.db;Version=3;New=False;Compress=True;");
+        }
+
+        private void ExecuteQuery(string txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+        private void LoadDate()
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select * from test";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            dataGridView1.DataSource = DT;
+            sql_con.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDate();
+        }
+        private void add_Click(object sender, EventArgs e)
+        {
+            string txtQuery = "insert into test(price,name) values ('" + id_txt.Text + "','" + name_txt.Text + "')";
+            ExecuteQuery(txtQuery);
+            LoadDate();
+        }
+
+
+
+
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+
+            string txtQuery = "update test set name='" + name_txt.Text + "' where price = '" + id_txt.Text + "'";
+
+            ExecuteQuery(txtQuery);
+            LoadDate();
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            string txtQuery = "delete from test where price='" + id_txt.Text + "'";
+            ExecuteQuery(txtQuery);
+            LoadDate();
+        }
+    }
+}
